4eb37aeb3010957585cf83400af90bd8fcb4e355
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-08-10 14:51:37 +0300

    feat: Add monitoring module

---
+++ vars.tf
@@ -0,0 +1,44 @@
+variable tribe_project_id {
+  type        = string
+  description = "Project ID where the Monitoring resources will be created"
+}
+
+variable clan_project_id {
+  type        = string
+  description = "Clan Project ID"
+}
+
+variable clan_name {
+  type        = string
+  description = "The name of the clan"
+}
+
+variable notification_channels {
+  type        = list
+  description = "List of notificaton channel IDs"
+  default     = []
+}
+
+variable pub_sub_monitoring {
+    type        = bool
+    description = "If the Pub/Sub dashboards and alerts should be created"
+    default     = false
+}
+
+variable cloudrun_monitoring {
+    type        = bool
+    description = "If the Cloud Run dashboards and alerts should be created"
+    default     = false
+}
+
+variable firestore_monitoring {
+    type        = bool
+    description = "If the Firestore dashboards and alerts should be created"
+    default     = false
+}
+
+variable cloudfunction_monitoring {
+    type        = bool
+    description = "If the Cloud Function dashboards and alerts should be created"
+    default     = false
+}
