537278a6ff9175d30bcd2894366a2ae914e12879
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-08-10 15:17:17 +0300

    feat: Add examples

---
+++ examples/main.tf
@@ -0,0 +1,9 @@
+module monitoring {
+  source = "../"
+
+  tribe_project_id     = "tribe-project-id"
+  clan_project_id      = "clan-project-id"
+  clan_name            = "my-clan"
+  pubsub_monitoring    = true
+  firestore_monitoring = true
+}
