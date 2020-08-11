d460ef39b5dac61fca96a06dd032ce9f67cb4fad
Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>
2020-08-11 12:10:15 +0300

    add required providers block

--- providers.tf
+++ providers.tf
@@ -1,6 +1,9 @@
 terraform {
   # The configuration for this backend will be filled in by Terragrunt
   required_version = ">= 0.12.18"
+  required_providers {
+    google = "~> 3.8"
+  }
 }
 
 provider "google" {
