f21f8bda3aac786a01c2e25c4594ead2055064d7
akovtun-extenda <57349964+akovtun-extenda@users.noreply.github.com>
2020-08-13 15:54:45 +0300

    delete submodule (#5)
    Co-authored-by: Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>

--- modules/notification-channels/outputs.tf
+++
@@ -1,4 +0,0 @@
-output notification_channels {
-  description = "The list of notification channels"
-  value       = flatten([google_monitoring_notification_channel.clan_email.*.id, google_monitoring_notification_channel.clan_slack_channel.*.id])
-}
