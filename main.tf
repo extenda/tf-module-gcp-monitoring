8cb1db44b22a8624d620a1ff81032f6da05f046d
akovtun-extenda <57349964+akovtun-extenda@users.noreply.github.com>
2020-12-15 14:48:45 +0200

    feat: Add default alerts for pub/sub (#7)
    * feat: Add default alerts for pub/sub
    
    * fix value
    
    Co-authored-by: Anastasiia Kovtun <nastia@Anastasiias-MacBook-Pro-2.local>

--- main.tf
+++ main.tf
@@ -25,7 +25,78 @@ resource "google_monitoring_alert_policy" "pub_sub_alert_policy" {
       aggregations {
         alignment_period   = "60s"
         per_series_aligner = "ALIGN_COUNT"
-        group_by_fields    = ["resource.labels.name"]
+        group_by_fields    = ["resource.label.subscription_id"]
+      }
+    }
+  }
+}
+
+resource "google_monitoring_alert_policy" "pub_sub_alert_policy_acknowledgment" {
+  count = var.pubsub_monitoring ? 1 : 0
+
+  project               = var.tribe_project_id
+  notification_channels = var.notification_channels
+  display_name          = "Messages acknowledgment"
+  combiner              = "AND"
+  conditions {
+    display_name = "Messages acknowledgment"
+    condition_threshold {
+      threshold_value = "1"
+      filter          = "metric.type=\"pubsub.googleapis.com/subscription/num_undelivered_messages\" resource.type=\"pubsub_subscription\" resource.label.\"project_id\"=\"${var.clan_project_id}\""
+      duration        = "1800s"
+      comparison      = "COMPARISON_GT"
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_COUNT"
+        group_by_fields    = ["resource.label.subscription_id"]
+      }
+    }
+  }
+}
+
+resource "google_monitoring_alert_policy" "pub_sub_alert_policy_push_latency" {
+  count = var.pubsub_monitoring ? 1 : 0
+
+  project               = var.tribe_project_id
+  notification_channels = var.notification_channels
+  display_name          = "Push subscription latency"
+  combiner              = "AND"
+  conditions {
+    display_name = "Push subscription latency"
+    condition_threshold {
+      threshold_value = "3000000"
+      filter          = "metric.type=\"pubsub.googleapis.com/subscription/push_request_latencies\" resource.type=\"pubsub_subscription\" resource.label.\"project_id\"=\"${var.clan_project_id}\""
+      duration        = "120s"
+      comparison      = "COMPARISON_GT"
+      aggregations {
+        alignment_period     = "60s"
+        per_series_aligner   = "ALIGN_DELTA"
+        cross_series_reducer = "REDUCE_PERCENTILE_95"
+        group_by_fields      = ["resource.label.subscription_id"]
+      }
+    }
+  }
+}
+
+resource "google_monitoring_alert_policy" "pub_sub_alert_policy_response_status" {
+  count = var.pubsub_monitoring ? 1 : 0
+
+  project               = var.tribe_project_id
+  notification_channels = var.notification_channels
+  display_name          = "Response status codes 5xx"
+  combiner              = "AND"
+  conditions {
+    display_name = "Response status codes 5xx"
+    condition_threshold {
+      threshold_value = "0"
+      filter          = "metric.type=\"pubsub.googleapis.com/subscription/push_request_count\" resource.type=\"pubsub_subscription\" metric.label.\"response_class\"=\"remote_server_5xx\" metric.label.\"response_code\"!=\"unreachable_5xx_error_502\" resource.label.\"project_id\"=\"${var.clan_project_id}\""
+      duration        = "120s"
+      comparison      = "COMPARISON_GT"
+      aggregations {
+        alignment_period     = "60s"
+        cross_series_reducer = "REDUCE_PERCENTILE_05"
+        per_series_aligner   = "ALIGN_RATE"
+        group_by_fields      = ["resource.label.subscription_id", "metric.label.response_code"]
       }
     }
   }
