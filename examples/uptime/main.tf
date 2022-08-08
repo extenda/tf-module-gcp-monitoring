30300a7705a1d30a78a68c4c2e3a6e0d5e700b68
Anton-L-Ext <anton.lorge@extendaretail.com>
2022-08-08 13:20:29 +0200

    refactor: Example

--- examples/uptime/main.tf
+++ examples/uptime/main.tf
@@ -1,40 +1,20 @@
-module uptime_check {
-  source = "../../modules/uptime-checks"
-
-  project_id            = "clan-project-id"
+module "uptime-check" {
+  source                = "/Users/user/Desktop/gcp-monitoring/tf-module-gcp-monitoring/modules/uptime-check"
   monitoring_project_id = "monitoring-project-id"
-  project_env           = "prod"
-  clan_name             = "clan"
 
   uptime_checks = [
     {
-      service_name = "Service 1"
-      timeout      = "60s"
-      period       = "60s"
-      check_type   = "HTTPS"
-      type         = "uptime_url"
-      hostname     = "service1.test.com"
-      headers = {
-        "foo" = "bar"
-      }
+      service_name = "sre.my-app",
+      hostname     = "my-app.com",
     },
     {
-      service_name = "Service 2"
-      check_type   = "HTTP"
-      timeout      = "10s"
-      path         = "/healthz"
-      type         = "uptime_url"
-      password     = "securepassword"
-      username     = "user1"
-      hostname     = "service2.test1.com"
-    }
+      service_name = "sre.example",
+      hostname     = "example.com",
+      path         = "/healthz",
+    },
   ]
-
-  uptime_check_alerts = [
-    {
-      service_name    = "Service 1"
-      uptime_duration = "300s"
-      threshold       = 1
-    }
-  ]
+  labels = {
+    clan = "sre",
+    cc   = "1337"
+  }
 }
